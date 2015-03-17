35c4a5808a7224ebdf2379afd2f7b865a29ca8f1
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2015-03-17 21:38:50 +0100

    Added dependencies; added Bundle file

---
+++ src/KwetalStorageBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kwetal\StorageBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class KwetalStorageBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
